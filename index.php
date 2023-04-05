23af00215cc529da9e6e3e8aa3f904c8d6b119d2
Letibdi <114108449+Letibdi@users.noreply.github.com>
2023-04-04 22:02:31 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>Login da empresa</title>
+  </head>
+  <body>
+    <a href="./view/login.php">Logar funcionário</a><br>
+	</body>
+
+
+  <html>
+  <head>
+    <title>Clientes</title>
+  </head>
+  <body>
+    <a href="./view/UsuarioList.php">Lista de Clientes </a>
+	</body>
+
+</html>
